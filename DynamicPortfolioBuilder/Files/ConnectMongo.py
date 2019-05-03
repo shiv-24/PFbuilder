5c026d6446b69be47adc76f4928f00577bf53765
shiv-24 <shivaggarwal.shivam1107@gmail.com>
2019-05-02 21:22:22 -0400

    Added Index Data

--- DynamicPortfolioBuilder/Files/ConnectMongo.py
+++ DynamicPortfolioBuilder/Files/ConnectMongo.py
@@ -1,21 +1,10 @@
 from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
-from pprint import pprint
-# connect to MongoDB, change the << MONGODB URL  >> to reflect your own connection string
-# client = MongoClient('127.0.0.1:27017')
-# db=client.admin
-# Issue the serverStatus command and print the results
-# serverStatusResul=db.command("serverStatus")
-# pprint(serverStatusResul)
 
-# db = client.get_database('test_2');
-# print(db.get_collection('testing_1').find().next());
-
-def getMongoClient(ipAddress):
-    client= MongoClient(ipAddress)
+def getMongoClient(ipAddress) :
+    client = MongoClient(ipAddress)
     return client
 
-def getCollection(client,db_name,coll_name):
+def getcollection(client, db_name, coll_name):
     return client.get_database(db_name).get_collection(coll_name)
 
 def getCollDataFindOne(client,db_name,coll_name):
--- DynamicPortfolioBuilder/Files/ParseData.py
+++ DynamicPortfolioBuilder/Files/ParseData.py
@@ -1,22 +1,23 @@
-import ConnectMongo as MC
+import Files.ConnectMongo as MC
 # import datetime
 from dateutil.parser import parse
 import os
 import csv
 
 client = MC.getMongoClient('127.0.0.1:27017')
-path = "C:\\Coding Practice\\Stock Data"
-list_of_files={}
+# path = "C:\\Coding Practice\\Stock Data"
+path = "C:\\Coding Practice\\Index Data"
+list_of_files = {}
 for fileName in os.listdir(path):
-    if fileName[-4:]=='.csv':
-        list_of_files[fileName]= path+"\\"+fileName
-    with open(list_of_files[fileName],'r') as csv_file:
+    if fileName[-4:] == '.csv':
+        list_of_files[fileName] = path+"\\"+fileName
+    with open(list_of_files[fileName], 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
 
         count = 0
         header = ''
         ticker = ''
-        companyName= ''
+        companyName = ''
         for line in csv_reader:
             if count == 3:
                 header = line
@@ -26,22 +27,25 @@ for fileName in os.listdir(path):
                 ticker = line[0][line[0].find("(") + 1:line[0].find(")")]
                 start = 3
                 end = line[0].find("(")-1
-                companyName= line[0][start:end]
-            count+=1
+                companyName = line[0][start:end]
+            count += 1
         # print(line)
 
         count = 0
         for line in csv_reader:
-            if count>4:
+            if count > 4:
                 if not line:
                     break
-            MC.insertData(client,'market_data','stock_data',{header[0]:parse(line[0]),
-                                                             header[1]: float(line[1]),
-                                                             header[2]: float(line[2]),
-                                                             header[3]: float(line[3]),
-                                                             header[4]: float(line[4]),
-                                                             header[5]: float(line[5]),
-                                                             'Ticker':ticker,
-                                                             'CompanyName':companyName
-                                                            })
-            count+=1
+            #change collection name while loading stock data
+            MC.insertData(client, 'market_data','benchmark_data', {header[0]: parse(line[0]),
+                                                                header[1]: float(line[1]),
+                                                                header[2]: float(line[2]),
+                                                                header[3]: float(line[3]),
+                                                                header[4]: float(line[4]),
+                                                                # header[5]: float(line[5]), remove for loading stock
+                                                                # 'Ticker': ticker, remove for loading stock data
+                                                                # 'CompanyName': companyName remove for loading stock data
+                                                               'BenchmarkTicker':ticker, #comment this line while loading stock data
+                                                               'BenchmarkName':companyName #comment this line while loading stock data
+                                                                })
+            count += 1
